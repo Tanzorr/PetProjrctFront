a4d092d9a95db09f5451891efb0f1306980a6e2c
tanzorr <alexx1984@ukr.net>
2021-01-15 13:23:19 +0200

    Implement reducer and show products and single product with api, and redux3

--- src/Components/Comon/ProductItem.js
+++ src/Components/Comon/ProductItem.js
@@ -4,26 +4,28 @@ import './comon.module.css'
 
 const ProductItem = ({product}) => {
     console.log('products',product)
-    return <div className="product_item mb-5">
-        <NavLink to={`/product/${product.id}`}>
+    if(product){
+        return <div className="product_item mb-5">
+            <NavLink to={`/product/${product.id}`}>
+                <div className="row">
+                    <img className="img-rounded w-100 item_img"
+                         src={product.image}
+                         alt=""/>
+                </div>
+                <div className="row ">
+                    <h5 className='text-center pl-5 pr-5'>{product.title}</h5>
+                </div>
+            </NavLink>
             <div className="row">
-                <img className="img-rounded w-100 item_img"
-                     src={product.image}
-                     alt=""/>
+                <div className="w-100 text-center">
+                    <b className="price "><h2>{product.price}грн</h2></b>
+                </div>
             </div>
-            <div className="row ">
-                <h5 className='text-center pl-5 pr-5'>{product.title}</h5>
+            <div className="row justify-content-center ">
+                <button className="btn btn-primary">Add to card</button>
             </div>
-        </NavLink>
-        <div className="row">
-            <div className="w-100 text-center">
-                <b className="price "><h2>{product.price}грн</h2></b>
-            </div>
-        </div>
-        <div className="row justify-content-center ">
-            <button className="btn btn-primary">Add to card</button>
-        </div>
-    </div>;
+        </div>;
+    }
 };
 
 
